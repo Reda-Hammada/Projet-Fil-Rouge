bde9814c297a38af02171567b0845a420aefb123
Reda Hammada <hammada.reda.dev@gmail.com>
2022-05-31 23:36:20 +0200

    creating user method created

--- prototypes/prototype1(auth)/userManager.php
+++ prototypes/prototype1(auth)/userManager.php
@@ -17,6 +17,11 @@ class Usermanager {
 
         $fullName = $user->getFullName();
         $userName = $user->getUserName();
+        $email = $user->getEmail();
+        $password = $user->getPassword();
+        $query = "INSERT INTO users(user_name,full_name,email,pass_word) VALUES('?','?','?','?')";
+        $stmt = $this->connect->prepare($query);
+        $stmt->execute([$userName,$fullName,$email,$password]);
 
     }
 
