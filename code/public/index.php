5c5646ce871a7b4bd4ea2dbf413066466e4ccdf4
Reda Hammada <hammada.reda.dev@gmail.com>
2022-06-05 04:23:19 +0200

    working on responsive

--- code/public/index.php
+++ code/public/index.php
@@ -6,13 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="asset/style/style.css">
     <link rel="stylesheet" href="asset/style/flexboxgrid.css">
+    <link rel='stylesheet' href="asset/style/responsive.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
     <header>
         <nav>
             <div class='row'>
-                <div class="col-xs-6 col-md-2 col-lg-4 logo">
+                <div class="col-xs-9 col-md-2 col-lg-4 logo">
                     <h1>InvoiceMe</h1>
                 </div>
                 <div class="col-xs-6 col-lg-5 navLinks">
@@ -32,19 +34,39 @@
                     <button class='logIn'>Login</button>
                     <button class='signUp'>Sign up</button>
                 </div>
+                <div class='burgerNav col-xs-3'>
+                    <i class="fa-solid fa-bars"></i>
+                </dvi>
+
             <div>
         </nav>
     </header>
     <section class=' container heroSection'>
         <div class='row '>
-            <div class='col-lg-5 col-md-4'>
+            <div class='col-lg-5 col-md-4 freelancer'>
                     <h1>Are you a Freelancer ?</h1>
                 
-                    <p>Lorem ipsum dolor sit amet. At expedita voluptas non dignissimos exercitationem aut iste earum nam perspiciatis qui dignissimos magni.</P>
+                    <p >Lorem ipsum dolor sit amet. At expedita voluptas non dignissimos exercitationem aut iste earum nam perspiciatis qui dignissimos magni.</P>
                     <button class="getStarted">Gest started</button>
             </div>
-            <div class='col-lg-4 col-md-4'>
+            <div class='col-lg-4 col-md-4 heroImage '>
                 <img src="asset/images/heroSection.svg">
+            </div>
+        </div>
+    </section>
+    <section class='container whyChoose'>
+        <div class=" col-xs-12 col-lg-12  whyChooseH2">
+            <h2>Why to choose InvoiceMe </h2>
+        </div>
+        <div class=" col-xs-12 col-lg-12">
+            <p>Lorem ipsum dolor sit amet. At expedita voluptas non dignissimos exercitationem aut iste earum nam perspiciatis qui dignissimos magni.</P>
+
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="firstFeature"></div>
+                <div class="secondFeature"></div>
+                <div class="thirdFeature"></div>
             </div>
         </div>
     </section>
